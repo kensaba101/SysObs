84365bd7dc71be528cfd20fbabde5baaab3ca378
Ken Saba <kensaba@airmail.cc>
2021-02-15 07:18:21 +0800

    sup

--- examples/fiapp/sc_main.cpp
+++ examples/fiapp/sc_main.cpp
@@ -5,6 +5,8 @@
 
 #include "Vfiapp.h"
 
+#define SIMULATE_UNTIL_TIME 40
+
 int sc_main(int argc, char* argv[]) {
     // Prevent unused variable warnings
     if (false && argc && argv) {}
@@ -28,7 +30,7 @@ int sc_main(int argc, char* argv[]) {
     ios::sync_with_stdio();
 
     // Define clocks
-    sc_clock clk("clk", 10, SC_NS, 0.5, 3, SC_NS, true);
+    sc_clock clk("clk", 10, SC_NS, 0.5, 3, SC_NS, true); //true = posedge
     //sc_clock fastclk("fastclk", 2, SC_NS, 0.5, 2, SC_NS, true);
 
     // Define interconnect
@@ -52,23 +54,43 @@ int sc_main(int argc, char* argv[]) {
     top->o2(o2);
     top->o3(o3);
 
+    // Set some inputs
+    top->reset = 0; //reset is active high
+    top->clk = 0;
+    top->a = 0;
+    top->enable = 1; //active high
+
     // You must do one evaluation before enabling waves, in order to allow
     // SystemC to interconnect everything for testing.
     sc_start(1, SC_NS);
-
+    //1 nanosecond
 
     // Simulate until $finish
-    while (!Verilated::gotFinish()) {   
+    while (!Verilated::gotFinish() && (sc_time_stamp() < sc_time(SIMULATE_UNTIL_TIME, SC_NS))) {   
 
-        // Apply inputs
+        //toggle clk
+
+        // Apply inputs on negedge
         if (sc_time_stamp() > sc_time(1, SC_NS) && sc_time_stamp() < sc_time(10, SC_NS)) {
-            reset = !0;  // Assert reset
-            cout << top->o1 << top->o2 << top->o3 <<endl;  
-        } else {
-            reset = !1;  // Deassert reset
-            cout << top->o1 << top->o2 << top->o3 <<endl;
+            top->reset = !0;  // Assert reset
+            top->a = 1;
+            top->enable(1);
+        } else if(sc_time_stamp() >= sc_time(10, SC_NS) && sc_time_stamp() < sc_time(20, SC_NS)) {
+            top->reset = !1;  // Deassert reset
+            top->a = !top->a;
+            top->enable = 0;
+        } else if(sc_time_stamp() >= sc_time(20, SC_NS) && sc_time_stamp() < sc_time(30, SC_NS)) {
+            top->reset = !1;  // Deassert reset
+            top->a = !top->a;
+            top->enable = 1;
+        } else (sc_time_stamp() >= sc_time(20, SC_NS) && sc_time_stamp() < sc_time(40, SC_NS)) {
+            top->reset = !1;  // Deassert reset
+            top->a = !top->a;
+            top->enable = !top->enable;
         }
 
+        //print out what's relevant
+        cout << top->o1 << top->o2 << top->o3 <<endl;
         // Simulate 1ns 
         sc_start(1, SC_NS);
     }
