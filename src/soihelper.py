fdde0b936236521890ce7d45aad738e77fd9517f
Ken Saba <kensaba@airmail.cc>
2021-02-17 18:44:15 +0800

    Part of soihelper

---
+++ src/soihelper.py
@@ -0,0 +1,49 @@
+import csv
+import sys
+from shutil import copyfile
+import os
+cwd = os.getcwd()
+
+
+# soihelper is called (by sysobs program) with only two things, soi.csv file, and all RTL files (no cpp file)
+print( "Number of arguments:", len(sys.argv), "arguments." )
+argc = len(sys.argv)
+
+metacommentExample = "/*<--SOI-->*/"
+
+# If file extension == .sv or .v, create copy titled old[filename].sv or old[filename].v
+for x in range(argc):
+    inpFile = str(sys.argv[x])
+    print(inpFile)
+    fileCopy = "old_" + inpFile
+    # The following code can be reused
+    if (inpFile.lower()).endswith(('.sv', '.v')):
+        copyfile(cwd + "/" + inpFile, cwd + "/" + fileCopy)
+        print(fileCopy)
+
+"""
+
+with open('soi.csv') as csvfile:
+    csvReader = csv.reader(x, delimiter=',')
+    line_count = 0
+    for row in csvReader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+
+
+
+# Opens file called '[modulename].v/.sv, scans for every instance of signalname, inserts metacomment before semicolon
+def readcsv(moduleName, signalName):
+
+
+# Same as other readcsv function, but does so only for the specified line. 
+def readcsv(moduleName, signalName, line):
+
+
+"""
+
--- src/sysobs.cpp
+++ src/sysobs.cpp
@@ -1,4 +1,4 @@
-#include <string.h>
+#include <string>
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
@@ -7,7 +7,16 @@ void verilation(); //TODO:args, calls verilator with default verilator args, wit
 
 
 int main(int argc, char **argv){
+
+    //while loop for all flags and params to be processed
+    //user select DPI or VPI using flag
     
+
+    //pass verilator arguments to verilator, if none specified use default args
+    //sc (cpp) wrapper file is passed to verilator, this file is not processed by sysobs, so burden of making sure everything's ready for VPI/DPI usage is on user.
+    
+    //.v/.sv files are modified, for metacomments necessary for DPI/VPI, as well as commenting in/out only DPI or only VPI as specified by user. 
+    //SOI.csv is processed 
     
 }
 
