f1a718567b1a279da00adfbc505460a24652d06c
Ken Saba <kensaba@airmail.cc>
2021-02-20 04:57:30 +0800

    Add metacomment

--- src/soihelper.py
+++ src/soihelper.py
@@ -2,14 +2,61 @@ import csv
 import sys
 from shutil import copyfile
 import os
-cwd = os.getcwd()
+import re
 
+cwd = os.getcwd()
 
 # soihelper is called (by sysobs program) with only two things, soi.csv file, and all RTL files (no cpp file)
 print( "Number of arguments:", len(sys.argv), "arguments." )
 argc = len(sys.argv)
 
-metacommentExample = "/*<--SOI-->*/"
+metacomment = "/*<--SOI-->*/"
+insertBefore = ";"
+
+# Enable user to use csv files of names other than the default (soi.csv) 
+def getCsvName():
+    for x in range(argc):
+        inpFile = str(sys.argv[x])
+        if (inpFile.lower()).endswith(('.csv', '.CSV')):
+            return inpFile
+    return 'soi.csv'
+    
+
+csvFileName = getCsvName()
+
+def soiFirstAppearance(moduleName, soiName): 
+    with open(moduleName) as fp: # Open file moduleName
+        line = fp.readline() 
+        soiLineNum = 0
+        
+        while line: # Scan (top to EOF) for string match "soiName"
+            if(re.search(soiName, line)): # If soiName is found anywhere in line, return current line number
+                return soiLineNum
+            else: # Else, incrememnt soiLineNum and continue scanning
+                line = fp.readline() 
+                soiLineNum +=1
+        print('No match for SOI name "', soiName, '" in file "', modulename, '"')
+
+def insertMetacomment(moduleName, lineNum, insertBefore, metacomment):
+    #readFile  = open(cwd + '/' + moduleName, "r") # Open file with intention to get lines
+    readFile  = open(moduleName, "r")
+    newFileData = ""
+    lineCount = 0
+    for line in readFile:
+        if (lineCount == lineNum):
+            strippedLine = line.strip('\n')
+            newLine = strippedLine.replace(";", (metacomment + insertBefore))
+            newFileData += newLine + "\n"
+            lineCount += 1
+        else:
+            newFileData += line
+            lineCount += 1
+    readFile.close()
+
+    #writeFile = open(cwd + '/' + moduleName, "w")
+    writeFile = open(moduleName, "w")
+    writeFile.write(newFileData)
+    writeFile.close() # Close file
 
 # If file extension == .sv or .v, create copy titled old[filename].sv or old[filename].v (for backup)
 for x in range(argc):
@@ -21,29 +68,20 @@ for x in range(argc):
         copyfile(cwd + "/" + inpFile, cwd + "/" + fileCopy)
         print(fileCopy)
 
-"""
-
-with open('soi.csv') as csvfile:
-    csvReader = csv.reader(x, delimiter=',')
-    line_count = 0
-    for row in csvReader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
+# RTL file modification (metacomment insertion)
+with open(cwd + '/' + csvFileName) as csvFile:
+    csvReader = csv.reader(csvFile, delimiter=',')
+    lineCount = 0; 
+    for row in csvReader: #while (!EOF), NTS: rows indexed from 0 not 1
+        moduleName = row[0]
+        print(moduleName)
+        soiName = row[1]
+        print(soiName)
+        if (len(row) != 3): 
+            lineNum = soiFirstAppearance(moduleName, soiName) #function might return wrong line (eg if user uses soiname in a comment in RTL file)
+            lineCount +=1
         else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
-
-
-
-# Opens file called '[modulename].v/.sv, scans for every instance of signalname, inserts metacomment before semicolon
-def readcsv(moduleName, signalName):
-
-
-# Same as other readcsv function, but does so only for the specified line. 
-def readcsv(moduleName, signalName, line):
-
-
-"""
-
+            lineNum = int(row[2])
+            print(lineNum)
+            lineCount +=1
+        insertMetacomment(moduleName, lineNum, insertBefore, metacomment)
