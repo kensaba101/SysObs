ef5b105dc9c651eef88440d880f867fd9e053c42
Ken Saba <kensaba@airmail.cc>
2021-03-09 21:52:08 +0800

    sup

--- examples/fiapp/sc_vpi.cpp
+++ examples/fiapp/sc_vpi.cpp
@@ -121,14 +121,14 @@ int sc_main(int argc, char** argv) {
             //TODO: Fix issue with value injected by setSoiValue not persisting after posedge clk (after sc_start)
             cout << "q1, q2, q3 before calling sc_start(): " << endl; 
             cout << getSoiValue(vhq1) << getSoiValue(vhq2) << getSoiValue(vhq3) << endl;
-            cout << "o1, o2, o3 before calling sc_start(): "
+            cout << "o1, o2, o3 before calling sc_start(): " << endl;
             cout << o1 << o2 << o3 << endl; 
             // Evaluates model & progresses clock by 1 ns
             sc_start(1, SC_NS);
             
             cout << "q1, q2, q3 after calling sc_start()" << endl; 
             cout << getSoiValue(vhq1) << getSoiValue(vhq2) << getSoiValue(vhq3) << endl;
-            cout << "o1, o2, o3 before calling sc_start(): "
+            cout << "o1, o2, o3 before calling sc_start(): " << endl;
             cout << o1 << o2 << o3 << endl; 
             cout << "[" << sc_time_stamp().value() << "] " << " clk=" << clk << " reset=" << reset << " enable=" << enable << " a=" << a << " o1=" << o1 
                 << " o2=" << o2 << " o3=" << o3 << endl;
