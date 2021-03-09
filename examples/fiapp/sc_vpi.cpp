3f23f3ba384e9660684517dfc99d73f422fc0631
Ken Saba <kensaba@airmail.cc>
2021-03-09 21:59:11 +0800

    sup

--- examples/fiapp/sc_vpi.cpp
+++ examples/fiapp/sc_vpi.cpp
@@ -122,7 +122,6 @@ int sc_main(int argc, char** argv) {
             cout << "q1, q2, q3 before calling sc_start(): " << endl; 
             cout << getSoiValue(vhq1) << getSoiValue(vhq2) << getSoiValue(vhq3) << endl;
             cout << "o1, o2, o3 before calling sc_start(): " << endl;
-            top->eval(); 
             cout << o1 << o2 << o3 << endl; 
             // Evaluates model & progresses clock by 1 ns
             sc_start(1, SC_NS);
