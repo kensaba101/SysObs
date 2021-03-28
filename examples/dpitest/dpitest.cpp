6f3154e259a003a2fde9ee9a4c32d0d06e267be8
Ken Saba <kensaba@airmail.cc>
2021-03-29 06:42:58 +0800

    sup

--- examples/dpitest/dpitest.cpp
+++ examples/dpitest/dpitest.cpp
@@ -7,53 +7,58 @@
 int add(int a, int b) { return a+b; }
 
 int dpic_line() { //works
-// Get a scope:  
-svScope scope = svGetScope();
+    svScope scope = svGetScope();
 
-const char* scopenamep = svGetNameFromScope(scope);
-assert(scopenamep);
+    const char* scopenamep = svGetNameFromScope(scope);
+    assert(scopenamep);
 
-const char* filenamep = "";
-int lineno = 0;
-if (svGetCallerInfo(&filenamep, &lineno)) {
-    printf("dpic_line called from scope %s on line %d\n",
-        scopenamep, lineno);
-    return lineno;
-} else {
-    return 0;
+    const char* filenamep = "";
+    int lineno = 0;
+    if (svGetCallerInfo(&filenamep, &lineno)) {
+        printf("dpic_line called from scope %s on line %d\n",
+            scopenamep, lineno);
+        return lineno;
+    } else {
+        return 0;
+    }
 }
+
+//imports, called on sv side. 
+void testValRead1(){
+    cout << "testValRead1 form c called.";
+    testvalVal = 1; 
 }
+void testValRead0(){
+    cout << "testValRead0 form c called.";
+    testvalVal = 2; 
+} 
+
+vluint64_t main_time = 0;       // Current simulation time
+// This is a 64-bit integer to reduce wrap over issues and
+// allow modulus.  This is in units of the timeprecision
+// used in Verilog (or from --timescale-override)
 
 
 
-        //Vdpitest *dpitest;                      // Instantiation of module
-
-        vluint64_t main_time = 0;       // Current simulation time
-        // This is a 64-bit integer to reduce wrap over issues and
-        // allow modulus.  This is in units of the timeprecision
-        // used in Verilog (or from --timescale-override)
-
-
-
-        int sc_main(int argc, char** argv) {    
-            Verilated::commandArgs(argc, argv);   // Remember args
-
-            Vdpitest* top;
-            top = new Vdpitest("top");
-            sc_clock clk("clk", 2, SC_NS, 0.5, true);
-            top->clk(clk); 
-            svLogic test = 1; 
-            sc_start(1, SC_NS); 
-            svSetScope(svGetScopeFromName("top.dpitest"));
-            cout << "svLogic print test" << test << endl; 
-            while (!Verilated::gotFinish() && (sc_time_stamp() < sc_time(50, SC_NS)) ) {
-                //cout << "value of testval address: " << testvalPtr << endl; 
-                getTestval(); 
-                setTestval1(); 
+int sc_main(int argc, char** argv) {    
+    Verilated::commandArgs(argc, argv);   // Remember args
+    int testvalVal; 
+    Vdpitest* top;
+    top = new Vdpitest("top");
+    sc_clock clk("clk", 2, SC_NS, 0.5, true);
+    top->clk(clk); 
+    svLogic test = 1; 
+    sc_start(1, SC_NS); 
+    svSetScope(svGetScopeFromName("top.dpitest"));
+    cout << "svLogic print test" << test << endl; 
+    while (!Verilated::gotFinish() && (sc_time_stamp() < sc_time(50, SC_NS)) ) {
+        //cout << "value of testval address: " << testvalPtr << endl; 
+        getTestval(); 
+        setTestval1(); 
             
-                sc_start(1, SC_NS); 
-            }
+        sc_start(1, SC_NS); 
+    }
 
-            top->final();               // Done simulating
-            return 0; 
-        }
+    top->final(); // Done simulating
+    return 0; 
+}
--- examples/dpitest/dpitest.sv
+++ examples/dpitest/dpitest.sv
@@ -8,13 +8,15 @@ module dpitest
     //import "DPI-C" function int add (input int a, input int b);
     //export "DPI-C" function integer \$myRand;
     import "DPI-C" context function int dpic_line();
+    import "DPI-C" context function void testValRead1();
+    import "DPI-C" context function void testValRead0();
 
     //import "DPI-C" context function logic getSoiHandle(input logic soi); //input logic -> svLogic, output logic -> svlogic*
     //export "DPI-C" function sendTvHandle; 
     //export "DPI-C" function setTestval; 
     export "DPI-C" function getTestval; 
     export "DPI-C" function setTestval1; 
-    //export "DPI-C" function setTestval0; 
+    export "DPI-C" function setTestval0; 
 
     //import "DPI-C" context function void setSoi(input logic value, output logic soi);
 
@@ -41,8 +43,15 @@ module dpitest
     */
     
     function getTestval();
-        $display (" Clk: %d, Testval : %d", clk, testval); //works
+        //$display (" Clk: %d, Testval : %d", clk, testval); //works
         //call import here to send value over to c-side (read) 
+        if (testval == 1'b1) begin
+            $display ("sv function getTestval called, calling c import testvalread1: ";
+            testValRead1();
+        end else begin
+            $display ("sv function getTestval called, calling c import testvalread0: ";
+            testValRead0();
+        end
     endfunction
 
     function setTestval1();
@@ -51,13 +60,13 @@ module dpitest
         $display (" Clk: %d, Testval : %d", clk, testval);
     endfunction
 
-    /*
+    
     function setTestval0(); 
         $display (" Clk: %d, Testval : %d", clk, testval);
         testval = 0; 
         $display (" Clk: %d, Testval : %d", clk, testval);
     endfunction
-    */
+    
 
     /*
     function logic getTestval;
