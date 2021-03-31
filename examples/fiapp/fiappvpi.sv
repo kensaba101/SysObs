6c1b3a901cb77a5bbde909fa1808b2581829ba4e
Ken Saba <kensaba@airmail.cc>
2021-04-01 00:34:45 +0800

    sup

--- examples/fiapp/fiappvpi.sv
+++ examples/fiapp/fiappvpi.sv
@@ -11,9 +11,9 @@ module fiappvpi
 );
 
     //Global switch (--public-flat-rw can be used to turn everything in sim accessible by VPI, but will reduce performance and possibly cause clock  mis-simulation)
-    logic q1 /*verilator public_flat_rd*/; //q1 is read only
-    logic q2 /*verilator public_flat_rw @(posedge clk) */; //q2 is write only
-    logic q3 /*verilator public_flat*/; //read or write by using c code
+    logic q1 /*verilator public_flat*/; //q1 is read only
+    logic q2 /*verilator public_flat*/; //q2 is read/write
+    logic q3 /*verilator public_flat*/; //q3 is read/write
 
     assign o1 = q1; 
     assign o2 = q2; 
